0464f1dfc1322a6c9dc68a353aaf72eecd6bbd7c
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-06 07:58:32 +0100

    Update: advanced task #102 complete

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Annotation challenge
+"""
+from typing import Tuple, List, Optional
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Annotation challenge
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(tuple(array))
+
+zoom_3x = zoom_array(tuple(array), int(3.0))
