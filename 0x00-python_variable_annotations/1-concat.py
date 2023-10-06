682efea0b7ff5672b19e3fc83c7049c72649f153
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-06 06:13:28 +0100

    Update: task #1 complete

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+1-concat.py
+
+Contains the function concat() which accepts two strings,
+concatenates them and returns the result.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat(str1, str2)
+
+    Args:
+      - str1 -> string argument
+      - str2 -> string argument
+
+    Return:
+      - result of concatenating str1 & str2
+    """
+    return str1 + str2
