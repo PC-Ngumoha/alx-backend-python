b5eea0f6216fcaab5339e420b25e90f3393bdd16
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-06 05:57:03 +0100

    Update: task #0 complete

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+0-add.py
+
+Creates a function that takes in two float args and
+returns their sum (another float)
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    add(a, b)
+
+    Args:
+      - a -> floating point number
+      - b -> floating point number
+
+    Return:
+      - sum of a & b -> floating point number
+    """
+    return a + b
