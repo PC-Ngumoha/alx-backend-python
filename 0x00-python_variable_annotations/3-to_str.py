20d949e7d88308efc9d8856fba607ceb44e23e90
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-06 06:41:19 +0100

    Update: task #3 complete

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+3-to_str.py
+
+Contains the definition of the to_str() function which accepts
+a floating point number 'n' and returns its string representation
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    to_str(n)
+
+    Args:
+      - n -> floating point argument
+
+    Return:
+      - str(n) -> string representation of n
+    """
+    return str(n)
