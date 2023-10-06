3f162c4f7600404945a70d7d0e2cc62060c8798a
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-06 06:34:54 +0100

    Update: task #2 complete

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+2-floor.py
+
+Contains the floor() which takes a floating point number and
+returns the floor (Integer less than or equal to that number)
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    floor(n)
+
+    Args:
+      - n -> floating point number
+
+    Return:
+      - math.floor(n)
+    """
+    return math.floor(n)
