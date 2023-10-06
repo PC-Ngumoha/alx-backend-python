62b981971be786d5142dd2c64b339ad5bbfb09d4
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-06 06:45:46 +0100

    Update: task #4 complete

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Contains the annotated definitions of a set of variables
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
