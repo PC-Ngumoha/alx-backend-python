f63a2b3ec33a59f0cf125a1f3cbfbf90b68fe9b1
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-29 09:32:54 +0100

    Update: Add new tests to test file: test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,3 +22,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """test_access_nested_map test function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ('a',)),
+        ({'a': 1}, ('a', 'b'))
+    ])
+    def test_access_nested_map_exception(self, map, path):
+        """test_access_nested_map_exception test function"""
+        with self.assertRaises(KeyError):
+            access_nested_map(map, path)
